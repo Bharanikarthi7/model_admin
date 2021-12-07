29888c68f4a88d5dd29cbec9bc98d5e63ba686c4
Bharani <Bharani.r@shoppro.tech>
2021-12-07 18:12:05 +0530

    order

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { render } from 'react-dom';
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  gql
+} from "@apollo/client";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+const client = new ApolloClient({
+  uri: 'http://localhost/wordpress/graphql',
+  cache: new InMemoryCache()
+});
+
+const GetOrders = gql`
+  query orders {
+    nodes {
+      orderNumber
+      date
+      status
+      total
+      customer {
+        firstName
+        lastName
+      }
+    }
+  }
+`;
+
+function ExchangeRates() {
+  const { loading, error, data } = useQuery(GetOrders);
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error' ${error.message}'</p>;
+  console.log(data);
+
+  // return data.orders.nodes.map(({ orderNumber}) => (
+  //   <div key={orderNumber}>
+  //     <p>
+  //       {orderNumber}
+  //     </p>
+  //   </div>
+  // ));
+}
+
+render(
+  <ApolloProvider client={client}>
+    <ExchangeRates />
+  </ApolloProvider>,
+  document.getElementById('root'),
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
